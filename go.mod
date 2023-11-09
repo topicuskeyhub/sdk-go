abbae415279371c93449121b48c67674e12a7c97
Emond Papegaaij <emond.papegaaij@topicus.nl>
2023-11-09 15:28:08 +0100

    Add support for device code flow

--- go.mod
+++ go.mod
@@ -7,22 +7,21 @@ require (
 	github.com/microsoft/kiota-abstractions-go v1.4.0
 	github.com/microsoft/kiota-serialization-json-go v1.0.4
 	golang.org/x/oauth2 v0.13.0
+	github.com/coreos/go-oidc/v3 v3.7.0
 )
 
 require (
+	github.com/go-jose/go-jose/v3 v3.0.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
-	github.com/pquerna/cachecontrol v0.2.0 // indirect
 	github.com/std-uritemplate/std-uritemplate/go v0.0.46 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
-	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 )
 
 require (
 	github.com/cjlapao/common-go v0.0.39 // indirect
-	github.com/coreos/go-oidc v2.2.1+incompatible
 	github.com/go-logr/logr v1.3.0 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/microsoft/kiota-http-go v1.1.0
--- key_hub_client_adapter.go
+++ key_hub_client_adapter.go
@@ -1,3 +1,6 @@
+// Copyright (c) Topicus Security B.V.
+// SPDX-License-Identifier: APSL-2.0
+
 package sdkgo
 
 // not-generated
@@ -10,7 +13,7 @@ import (
 	"net/url"
 	"time"
 
-	"github.com/coreos/go-oidc"
+	"github.com/coreos/go-oidc/v3/oidc"
 	auth "github.com/microsoft/kiota-abstractions-go/authentication"
 	http "github.com/microsoft/kiota-http-go"
 
@@ -69,13 +72,62 @@ func NewKeyHubRequestAdapter(client *nethttp.Client, issuer string, clientID str
 		return nil, err
 	}
 
-	appClientConf := clientcredentials.Config{
+	oauth2Conf := clientcredentials.Config{
 		ClientID:     clientID,
 		ClientSecret: clientSecret,
 		Scopes:       []string{oidc.ScopeOpenID},
 		TokenURL:     provider.Endpoint().TokenURL + "?authVault=access",
 	}
-	tokenSource := appClientConf.TokenSource(ctx)
+	tokenSource := oauth2Conf.TokenSource(ctx)
+	accessTokenProvider := NewKeyHubAccessTokenProvider(&tokenSource)
+	middlewares := []http.Middleware{
+		accessTokenProvider,
+		http.NewParametersNameDecodingHandler(),
+		http.NewUserAgentHandler(),
+	}
+	client.Transport = http.NewCustomTransportWithParentTransport(client.Transport, middlewares...)
+	authProvider := auth.NewBaseBearerTokenAuthenticationProvider(accessTokenProvider)
+	adapter, err := http.NewNetHttpRequestAdapterWithParseNodeFactoryAndSerializationWriterFactoryAndHttpClient(authProvider, nil, nil, client)
+	if err != nil {
+		return nil, err
+	}
+	adapter.SetBaseUrl(issuer + "/keyhub/rest/v1")
+	return adapter, nil
+}
+
+func NewKeyHubRequestAdapterForDeviceCode(client *nethttp.Client, issuer string, clientID string, clientSecret string, scopes []string) (*http.NetHttpRequestAdapter, error) {
+	if client == nil {
+		client = nethttp.DefaultClient
+	}
+	if client.Timeout == 0 {
+		client.Timeout = time.Second * 20
+	}
+	ctx := oidc.ClientContext(context.Background(), client)
+	provider, err := oidc.NewProvider(ctx, issuer)
+	if err != nil {
+		return nil, err
+	}
+
+	oauth2Conf := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Scopes:       scopes,
+		Endpoint:     provider.Endpoint(),
+	}
+	oauth2Conf.Endpoint.DeviceAuthURL = issuer + "/login/oauth2/authorizedevice"
+
+	response, err := oauth2Conf.DeviceAuth(ctx, oauth2.SetAuthURLParam("authVault", "access"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("please enter code %s at %s\n", response.UserCode, response.VerificationURIComplete)
+	token, err := oauth2Conf.DeviceAccessToken(ctx, response)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(token)
+
+	tokenSource := oauth2Conf.TokenSource(ctx, token)
 	accessTokenProvider := NewKeyHubAccessTokenProvider(&tokenSource)
 	middlewares := []http.Middleware{
 		accessTokenProvider,
